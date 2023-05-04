fd237e1fd2b64b8e440909d963d27b81640b6b17
probitiye <vm683497580@gmail.com>
2023-05-04 12:02:26 +0300

    added tray/registry handler

---
+++ lbregistry/lbregistry.cpp
@@ -0,0 +1,96 @@
+﻿#include "pch.h"
+
+
+// Прототипи функцій
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+void AddTrayIcon(HWND hwnd);
+void RemoveTrayIcon(HWND hwnd);
+
+// Глобальні змінні
+HINSTANCE hInst;
+NOTIFYICONDATA nid;
+
+int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPTSTR    lpCmdLine,
+    _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    MSG msg;
+    WNDCLASSEX wcex;
+    ZeroMemory(&wcex, sizeof(WNDCLASSEX));
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.lpfnWndProc = WndProc;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
+    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszClassName = _T("RegistryTrayApp");
+
+    RegisterClassEx(&wcex);
+
+    HWND hWnd = CreateWindow(_T("RegistryTrayApp"), _T("Registry Tray App"), WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+
+    if (!hWnd)
+        return FALSE;
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_CREATE:
+        AddTrayIcon(hWnd);
+        break;
+
+    case WM_APP: // Callback message for tray icon
+        if (lParam == WM_LBUTTONUP)
+        {
+            MessageBox(hWnd, L"Left button clicked on tray icon", L"Tray Icon", MB_OK);
+        }
+        break;
+
+    case WM_DESTROY:
+        RemoveTrayIcon(hWnd);
+        PostQuitMessage(0);
+        break;
+
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+void AddTrayIcon(HWND hWnd)
+{
+    ZeroMemory(&nid, sizeof(NOTIFYICONDATA));
+
+    nid.cbSize = sizeof(NOTIFYICONDATA);
+    nid.hWnd = hWnd;
+    nid.uID = 1;
+    nid.uFlags = NIF_ICON | NIF_MESSAGE
+        | NIF_TIP;
+    nid.uCallbackMessage = WM_APP;
+    nid.hIcon = LoadIcon(hInst, IDI_APPLICATION);
+    wcscpy_s(nid.szTip, L"Registry Tray App"); Shell_NotifyIcon(NIM_ADD, &nid);
+}
+
+void RemoveTrayIcon(HWND hWnd)
+{
+    Shell_NotifyIcon(NIM_DELETE, &nid);
+}
