221890883d84474d6dfe792228e895d0471b9e7c
probitiye <vm683497580@gmail.com>
2023-05-04 11:02:34 +0300

    websocket sending works but there is a strange debugbreak on return 0; in lbdll-test:main()

--- lbdll-test/main.cpp
+++ lbdll-test/main.cpp
@@ -1,23 +1,38 @@
 #include "../shared/stb_image.h"
 #include "../shared/lbdll.h"
 #include <iostream>
+#include <thread>
 
 #define LB_IMG "./testimg.jpg" // in build directory
+
+#define LB_ASSERT(statement, msg_on_fail) \
+	if (!(statement)) \
+	{\
+		std::cout << (msg_on_fail) << "\n";\
+		std::terminate();\
+	}
+
+
 
 int main()
 {
 	int h{}, w{}, channels{};
 	auto img = stbi_load(LB_IMG, &w, &h, &channels, 0);
 
+
 	// test sending
-	// TODO use gtest / or custom header testing
-	// TODO in a thread and receive in the second thread
-	//send_image("127.0.0.1", 8888, img, w, h, channels);
-	
-	//int r_h{}, r_w{}, r_channels{}; // r_ means received
-	//auto recv_img = receive_image(8888, &r_w, &r_h, &r_channels);
+
+	// run sending in separate thread but receive in current
+	std::thread t1{ send_image , "127.0.0.1", 8888, img, w, h, channels };
+
+	int r_h{}, r_w{}, r_channels{}; // r_ means received
+	auto recv_img = receive_image(8888, &r_w, &r_h, &r_channels);
+
+	LB_ASSERT(memcmp(img, recv_img, w*h) == 0, "sending by localhost failed - resulting image is not equal to the origin");
+
 
 	// test processing
+
 	process_image(img, w, h, channels,
 		[](auto& r, auto& g, auto& b)
 		{
